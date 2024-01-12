74fd63996af6acad375e5b1efba2715120510582
Babu <babu.gangatharan@ing.com>
2024-01-12 16:12:28 +0100

    Initial Commit

---
+++ AppFrameworks/AppFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFrameworksApp.swift
+//  AppFrameworks
+//
+//  Created by Babu Gangatharan on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
